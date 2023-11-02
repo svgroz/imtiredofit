5ebdfe71892a8e13c52f30fda883bd71f2e8ecfd
Simon Grozovsky <svgroz@outlook.com>
2023-11-02 21:40:15 +0400

    init

---
+++ src/main/java/org/svgroz/imtiredoftech/ImtiredoftechApplication.java
@@ -0,0 +1,13 @@
+package org.svgroz.imtiredoftech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImtiredoftechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImtiredoftechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/svgroz/imtiredoftech/ImtiredoftechApplicationTests.java
@@ -0,0 +1,13 @@
+package org.svgroz.imtiredoftech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImtiredoftechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
